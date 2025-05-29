a64a56af849f5abd461a186070acc061a50cadbb
Franciuto <francesco.traversetolo@gmail.com>
2025-05-29 17:06:44 +0200

    Improved documentation for level3

--- level3/level3_network.c
+++ level3/level3_network.c
@@ -1,4 +1,13 @@
-/* C-ISO-OSI - Network layer - Functions - Franciuto */
+/* C-ISO-OSI - Network Layer - Franciuto
+ *
+ * Handles the routing of data packets across networks.
+ *   - Adding source and destination IP addresses to the PDU
+ *   - Establishing logical connections between network nodes
+ *
+ * In this implementation, the layer receives data from upper layers,
+ * adds network headers, and passes it down
+ */
+
 // Library libraries
 #include "constants.h" // Library constants
 #include "level3_network.h"
@@ -13,7 +22,7 @@
     Takes the value passed as input and sends it to layer 2 using its send function
     -- INPUT --
        -> Data to send to layer 2
-    
+
     -- OUTPUT --
        -> Sends the data to layer 2
 */
@@ -24,42 +33,44 @@ char *livello3_send(const char *dati)
 }
 
 /* Funzione - livello3_send()
-    Function that adds the network header with hard coded IPs 
+    Function that adds the network header with hard coded IPs
     -- INPUT --
        -> PDU from level2
-    
+
     -- OUTPUT --
        -> Sends the final PDU to higher levels
 */
-char* livello3_receive(const char* pdu) {
+char *livello3_receive(const char *pdu)
+{
     // Get datas from level 2
-    char* data_from_l2 = livello2_receive(pdu);
-    
+    char *data_from_l2 = livello2_receive(pdu);
+
     // Check if no datas from l2
-    if (data_from_l2 == NULL) {
+    if (data_from_l2 == NULL)
+    {
         printf("[L3] Network - No data received from Layer 2\n");
         return NULL;
     }
-    
+
     // Network header defintion (hard coded IPs only)
-    const char* header = "[SRC=192.168.1.7] [DST=192.168.1.45] ";
-    
+    const char *header = "[SRC=192.168.1.7] [DST=192.168.1.45] ";
+
     // Size calcs
     int header_len = strlen(header);
     int data_len = strlen(data_from_l2);
     int total_len = header_len + data_len + 1; // +1 for null byte terminator
-    
-    char* network_pdu = (char*)malloc(total_len);
+
+    char *network_pdu = (char *)malloc(total_len);
 
     // Construct the final PDU
     strcpy(network_pdu, header);
     strcat(network_pdu, data_from_l2);
-    
+
     // Debug print
     printf("[L3] Network - Sending data to upper layers: \"%s\"\n", network_pdu);
-    
+
     // Free the data received from Layer 2
     free(data_from_l2);
-    
+
     return network_pdu;
 }
--- level3/level3_network.h
+++ level3/level3_network.h
@@ -4,7 +4,24 @@
 #ifndef LEVEL3_NETWORK_H
 #define LEVEL3_NETWORK_H
 
-char* livello3_send(const char* dati);
-char* livello3_receive(const char* pdu);
+/* Funzione - livello3_send()
+    Function that adds the network header with hard coded IPs
+    -- INPUT --
+       -> PDU from level2
+
+    -- OUTPUT --
+       -> Sends the final PDU to higher levels
+*/
+char *livello3_send(const char *dati);
+
+/* Function - livello3_send()
+    Takes the value passed as input and sends it to layer 2 using its send function
+    -- INPUT --
+       -> Data to send to layer 2
+
+    -- OUTPUT --
+       -> Sends the data to layer 2
+*/
+char *livello3_receive(const char *pdu);
 
 #endif
